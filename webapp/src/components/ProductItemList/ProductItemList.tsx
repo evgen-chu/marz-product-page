be580b9907f9e75d775df262d609d5e499581aec
evgeniia <evgeniia.chugreeva@gmail.com>
2023-03-05 18:10:35 -0500

    Add key to ProductItemList

--- webapp/src/components/ProductItemList/ProductItemList.tsx
+++ webapp/src/components/ProductItemList/ProductItemList.tsx
@@ -5,7 +5,11 @@ import ProductItem from '../ProductItem/ProductItem';
 const ProductItemList = (props: ProductItemListProps) => {
     const { products } = props;
     return (<div className="grid grid-cols-3 gap-10 mt-10 mr-20 ml-20" data-testid="product-list-container-div">
-        {products.map((item, ind) => (<ProductItem product={item} />))}
+        {products.map((item, ind) => (
+            <div key={ind}>
+                <ProductItem product={item} />
+            </div>
+))}
     </div>)
 }
 
